f449c840969f48adf471a219835d5d3b369fcab7
Abhi-0930 <139685267+Abhi-0930@users.noreply.github.com>
2024-10-10 21:41:35 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://abhishekj3094:abhi._.3094@cluster0.opjsx.mongodb.net/userInfo').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://aj309430:abhi._.3094@cluster0.toplc.mongodb.net/userInfo').then(()=>console.log("DB Connected"));
    
 }
