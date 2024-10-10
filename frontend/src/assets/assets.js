15ccd2e3a0696e440a166403fdee80ecc2a72d25
Abhi-0930 <139685267+Abhi-0930@users.noreply.github.com>
2024-10-11 00:25:08 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -103,10 +103,7 @@ export const menu_list = [
         menu_name: "Sandwich",
         menu_image: menu_4
     },
-    {
-        menu_name: "Cake",
-        menu_image: menu_5
-    },
+    
     {
         menu_name: "Pure Veg",
         menu_image: menu_6
