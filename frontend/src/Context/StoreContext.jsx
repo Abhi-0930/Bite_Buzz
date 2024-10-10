776343157e30b7613b3d106ba75a6a8162a82c4f
Abhi-0930 <139685267+Abhi-0930@users.noreply.github.com>
2024-10-10 23:59:51 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://bite-buzz-wspv.onrender.com"
+    const url = "https://bite-buzz-backend-khp4.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
